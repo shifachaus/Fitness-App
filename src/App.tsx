7d5d5ac5fde9741a6840e9dd0df1c30108a276f3
shifachaus <chausshifa804@gmail.com>
2024-02-28 13:13:28 +0530

    Add responsive navbar UI, toggle menu functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.screenY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+
+      if (window.screenX !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,23 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "@/shared/types";
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+function Link({ page, selectedPage, setSelectedPage }: Props) {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+	  transition duration-500 hover:text-primary-300`}
+    >
+      {page}
+    </AnchorLink>
+  );
+}
+
+export default Link;
